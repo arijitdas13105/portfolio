127ccbbfda65b4023a77d5daa420c5df964bf026
arijitdas13105 <arijitdas13105@gmail.com>
2024-01-20 20:55:28 +0530

    updating the projectt

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,14 +12,13 @@ const Projects = () => {
   const projectsData = [
     {
       id: 1,
-      imageSrc: images.eggSquare,
+      imageSrc:images.eggSquare,
       // imageSrc:
       //   "https://www.freevector.com/uploads/vector/preview/282/FreeVector-Cracked-Egg-Vector.jpg",
       projectUrl: "https://eggsquare.vercel.app/",
       projectName: "Egg Square",
       description:
-        "Developed NetflixGPT with precision, leveraging React, Firebase, Redux Toolkit, and Tailwind CSS. Seamlessly integrated GPT for personalized movie recommendations, prioritizing meticulous code structure, reusability, and maintainability.Elevating user experience through cutting-edge technology and meticulous development",
-      technologies: [
+      "I designed and deployed a MERN-based egg delivery platform, optimizing user registration, secure payments, real-time notifications, and responsive design, resulting in increased customer satisfaction. Utilized Node.js, Express.js, MongoDB, Razorpay, React, Redux, Axios, and CSS for full-stack development"      technologies: [
         "MongoDB",
         "React",
         "Express",
@@ -42,37 +41,45 @@ const Projects = () => {
     // ,
     {
       id: 3,
-      imageSrc: images.netflixGPT,
+      imageSrc:images.netflixGPT,
       // imageSrc:
       //   "https://www.fleetroot.com/wp-content/uploads/2020/08/how-to-create-a-powerful-car-rental-business-website-1.jpg",
       projectUrl: "https://netflixgptt.vercel.app/",
       projectName: "Netflix-GPT",
       description:
-        "Emphasized code structure, reusability, and maintainability. Elevating user experience through cutting-edge technology and meticulous development",
-      technologies: [
-        "React",
-        "Firebase",
-        "Redux Toolkit",
-        "Tailwind CSS",
-        "Responsive Design",
-      ],
+      "Developed NetflixGPT with precision, leveraging React, Firebase, Redux Toolkit, and Tailwind CSS. Seamlessly integrated GPT for personalized movie recommendations, prioritizing meticulous code structure, reusability, and maintainability.Elevating user experience through cutting-edge technology and meticulous development",
+      technologies:
+       [ "React",
+       "Firebase",
+       "Redux Toolkit",
+       "Tailwind CSS",
+       "Responsive Design",],
     },
   ];
   return (
     <>
+   
       <h1 className="heading-project">projects</h1>
       {/* <ProjectContainer description="sdds" technologies="d" /> */}
-      {projectsData.map((project) => (
-        <ProjectContainer
-          key={project.id}
-          imageSrc={project.imageSrc}
-          projectUrl={project.projectUrl}
-          projectName={project.projectName}
-          description={project.description}
-          technologies={project.technologies}
-        />
-      ))}
-    </>
+      {
+        projectsData.map((project)=>(
+         <ProjectContainer 
+         key={project.id}
+         imageSrc={project.imageSrc}
+         projectUrl={project.projectUrl}
+         projectName={project.projectName}
+         description={project.description}
+         technologies={project.technologies}
+          />
+
+
+        ))
+      }
+
+    
+</>
+
+    
   );
 };
 
