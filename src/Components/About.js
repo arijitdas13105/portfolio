7a0141ecbb45fe4ee915012f9fd063f9723fe88c
arijitdas13105 <arijitdas13105@gmail.com>
2024-04-11 09:54:59 +0530

    updating ui

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 import "./About.css";
 import { images } from "./images";
 import AutoType from "./AutoType";
-import './AutoType.css'
+import "./AutoType.css";
 
 import GraphemeSplitterComponent from "./GraphemeSplitterComponent";
 
@@ -21,34 +21,33 @@ const About = () => {
           Hi,I am Arijit Das, Full Stack Developer & final year student pursuing a combined degree in
           Information Technology and MBA from IIIT Gwalior. <br />
         </p> */}
-        <button className="btn" type="button" onClick={()=>{
-    window.open("https://drive.google.com/file/d/1FnjfhPU9Ay75JVuZwdcZc63hkceWpDBE/view?usp=sharing");
-  }}>
-  <strong>Resume</strong>
-  <div id="container-stars">
-    <div id="stars"></div>
-  </div>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/file/d/1FnjfhPU9Ay75JVuZwdcZc63hkceWpDBE/view?usp=sharing"
+            );
+          }}
+        >
+          <strong>Resume</strong>
+          <div id="container-stars">
+            <div id="stars"></div>
+          </div>
 
-  <div id="glow">
-    <div className="circle"></div>
-    <div className="circle"></div>
-  </div>
-</button>
-     <AutoType/>
-
+          <div id="glow">
+            <div className="circle"></div>
+            <div className="circle"></div>
+          </div>
+        </button>
+        <AutoType />
 
         {/* <p className="RRR">
           <h1>Hello, I am Arijit <br /></h1>
           <span>Full stack Developer || student @IIIT Gwalior</span>
           
         </p> */}
-
-
-
       </div>
-      
-     
-     
     </div>
   );
 };
